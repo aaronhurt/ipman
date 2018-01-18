4e13b9b06d4ef86b29a079689d3f570b6bd351c1
Aaron Hurt <ahurt@anbcs.com>
2018-01-17 22:15:37 -0600

    always helps to save the right buffer

--- common/dns/godaddy/models.go
+++ common/dns/godaddy/models.go
@@ -3,15 +3,15 @@ package godaddy
 // DNSRecord represents an element of the GoDaddy model
 // https://developer.godaddy.com/doc#!/_v1_domains/recordReplace/ArrayOfDNSRecord
 type DNSRecord struct {
-	Type     string `json:"type" validate:"required,eq=A|eq=AAAA|eq=CNAME|eq=MX|eq=NS|eq=SOA|eq=SRV|eq=TXT"`
-	Name     string `json:"name" validate:"required,min=1,max=255"`
-	Data     string `json:"data" validate:"required,min=1,max=255"`
-	Priority int    `json:"priority,omitempty" validate:"omitempty,gte=1"`
-	TTL      int    `json:"ttl" validate:"omitempty,gte=1"`
-	Service  string `json:"service,omitempty" validate:"omitempty,min=1"`
-	Protocol string `json:"protocol,omitempty" validate:"omitempty,min=1"`
-	Port     int    `json:"port,omitempty" validate:"omitempty,min=1,max=65535"`
-	Weight   int    `json:"weight,omitempty" validate:"omitempty,gte=1"`
+	Type     string  `json:"type" validate:"required,eq=A|eq=AAAA|eq=CNAME|eq=MX|eq=NS|eq=SOA|eq=SRV|eq=TXT"`
+	Name     string  `json:"name" validate:"required,min=1,max=255"`
+	Data     string  `json:"data" validate:"required,min=1,max=255"`
+	Priority *int    `json:"priority,omitempty" validate:"omitempty,gte=1"`
+	TTL      *int    `json:"ttl,omitempty" validate:"omitempty,gte=1"`
+	Service  *string `json:"service,omitempty" validate:"omitempty,min=1"`
+	Protocol *string `json:"protocol,omitempty" validate:"omitempty,min=1"`
+	Port     *int    `json:"port,omitempty" validate:"omitempty,min=1,max=65535"`
+	Weight   *int    `json:"weight,omitempty" validate:"omitempty,gte=1"`
 }
 
 // DNSRecords represents the GoDaddy model
